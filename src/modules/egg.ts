c8402731f57c843f240cabe79d3a1bc2c6625bc0
nowik <nowik249@gmail.com>
2023-08-05 14:30:48 +0200

    Setup initialize egg

---
+++ src/modules/egg.ts
@@ -0,0 +1,11 @@
+export enum EggState {
+    Egg = 'egg',
+    Tamagotchi = 'tamagotchi'
+}
+
+export class Egg {eggClicks: number = 0;
+    assets = new Map([
+        [EggState.Egg, "assets/egg.svg"],
+        [EggState.Tamagotchi, "assets/tamagotchi.svg"],
+    ]);
+}
